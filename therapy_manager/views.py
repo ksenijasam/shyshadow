221101640f51a1bf1428b7d1555db84973888683
ksenijasam <ksenija2702@gmail.com>
2023-07-13 20:39:11 +0200

    initial work on logging in

--- therapy_manager/views.py
+++ therapy_manager/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "therapy_manager/index.html", {})
+
+def login(request):
+    return render(request, "therapy_manager/login.html", {})
