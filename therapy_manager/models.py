047ed4596a9766553f76c089abb9286b1d147efa
ksenijasam <ksenija2702@gmail.com>
2023-07-13 22:25:18 +0200

    adding abstract user

--- therapy_manager/models.py
+++ therapy_manager/models.py
@@ -1,14 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
-    username =  models.CharField(max_length = 20)
+class User(AbstractUser):
     name =  models.CharField(max_length = 20)
-    name =  models.CharField(max_length = 30)
+    surname =  models.CharField(max_length = 30)
     is_therapist = models.BooleanField(default = False)
-    created_at = models.DateTimeField(auto_now_add = True, null = True)
 
-
-    def __str__(self):
-        return f"{self.name} {self.surname}, is therapist: {self.is_therapist}"
--- therapy_manager/views.py
+++ therapy_manager/views.py
@@ -1,9 +1,55 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 
 def index(request):
     return render(request, "therapy_manager/index.html", {})
 
-def login(request):
-    return render(request, "therapy_manager/login.html", {})
+def login_view(request):
+    if request.method == "POST":
+
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "therapy_manager/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "therapy_manager/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "therapy_manager/register.html", {
+                "message": "Passwords must match."
+            })
+
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "therapy_manager/register.html", {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "therapy_manager/register.html")
