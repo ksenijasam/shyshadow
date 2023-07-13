addd9d15db312003dc8ae3e2a9e88cdcc9f5c6fe
ksenijasam <ksenija2702@gmail.com>
2023-07-13 20:21:21 +0200

    added user model

--- therapy_manager/models.py
+++ therapy_manager/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username =  models.CharField(max_length = 20)
+    name =  models.CharField(max_length = 20)
+    name =  models.CharField(max_length = 30)
+    is_therapist = models.BooleanField(default = False)
+    created_at = models.DateTimeField(auto_now_add = True, null = True)
+
+
+    def __str__(self):
+        return f"{self.name} {self.surname}, is therapist: {self.is_therapist}"
