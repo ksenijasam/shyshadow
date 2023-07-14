060d3f2c6789551f7462e28a815b4d7ca5c53671
ksenijasam <ksenija2702@gmail.com>
2023-07-14 17:06:09 +0200

    adding appointment model

--- therapy_manager/models.py
+++ therapy_manager/models.py
@@ -8,3 +8,22 @@ class User(AbstractUser):
     surname =  models.CharField(max_length = 30)
     is_therapist = models.BooleanField(default = False)
 
+
+class Appointment(models.Model):
+    NEW = 'new'
+    CANCELED = 'canceled'
+    DONE = 'done'
+    CATEGORY_CHOICES = [
+        (NEW, 'new'),
+        (CANCELED, 'canceled'),
+        (DONE, 'done'),
+    ]
+
+    status = models.CharField(choices=CATEGORY_CHOICES, null=True, blank=True, max_length=10)
+    appointment_date = models.DateTimeField(auto_now_add = False, null = True)
+    goal = models.TextField()
+    client = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'client_appointment')
+    therapist = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'therapist_appointment')
+
+    def __str__(self):
+        return f"{self.client} has appointment {self.appointment_date} with {self.therapist}, status {self.status}"
