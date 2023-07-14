4ebbb7f6ec1e2e2d6a6619de436771345768fc81
ksenijasam <ksenija2702@gmail.com>
2023-07-14 16:04:44 +0200

    added logging out

--- therapy_manager/urls.py
+++ therapy_manager/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name = "index"),
     path("login_view", views.login_view, name = "login_view"),
     path("register", views.register, name = "register"),
+    path("logout_view", views.logout_view, name = "logout_view"),
 
 ]
